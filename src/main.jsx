a893e2ef44490c01fe8a5d934463290f511cd9ee
HarshalB84 <harshalbhoyar84@gmail.com>
2026-06-18 00:20:30 +0530

    Setting up the basic React Setup for AirBnb Clone

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { Button } from './components/ui/button'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <h1 className='bg-red-500 text-white '>Hello</h1>    
+    <div className='dark'>
+      <h1 className='bg-background text-foreground'>Hello</h1>
+      <Button>Click Me</Button>
+    </div>
+
   </StrictMode>,
 )
