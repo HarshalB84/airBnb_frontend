5a230ef14c7cbd332e26e5a11fd10840766a0666
Harshal Bhoyar <harshalbhoyar84@gmail.com>
2026-07-09 23:44:15 +0530

    Search Feature complete with DateSelctInput, LocationInput, OccupancyInput

--- src/feature/Search/Search.jsx
+++ src/feature/Search/Search.jsx
@@ -4,28 +4,46 @@ import LocationInput from "./LocationInput";
 import DateSelectInput from "./DateSelectInput";
 import { Button } from "@/components/ui/Button";
 import OccupancyInput from "./OccupancyInput";
+import dayjs from 'dayjs';
 
 const Search = () => {
 
-    const form = useForm();
+    const form = useForm({
+        defaultValues: {
+            city: '',
+            roomsCount: 1,
+            bookingDates: {
+            from: dayjs().toDate(),
+            to: dayjs().add(1, 'day').toDate()
+        }
+        },
+    });
 
-    function onSubmit(data){
-        console.log("Data : ",data);
-    }
+  function onSubmit(data) {
+    const sendData = {
+      city: data.city,
+      roomsCount: data.roomsCount,
+      startDate: dayjs(data.bookingDates.from).format('YYYY-MM-DD'),
+      endDate: dayjs(data.bookingDates.to).format('YYYY-MM-DD')
+    };
+    console.log('Search data', sendData);
+  }
 
     return (
-        <section className="container">
-            <Form {...form}>
-                <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-1 p-1 bg-yellow-500 
-                lg:flex-row lg:items-center rounded h-14">
-                    <LocationInput form={form}/>
-                    <DateSelectInput form={form}/>
-                    <OccupancyInput form={form}/>
-                    <Button type="submit" className="text-lg h-full">Search</Button>
-                </form>
-            </Form>
-        </section>
-    )
+    <section className="container">
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex flex-col gap-1 p-1 bg-yellow-500 lg:flex-row lg:items-center rounded h-16"
+        >
+          <LocationInput form={form}/>
+          <DateSelectInput form={form}/>
+          <OccupancyInput form={form}/>
+          <Button type="submit" className="text-lg h-full">Search</Button>
+        </form>
+      </Form>
+    </section>
+  );
 }
 
 export default Search;
