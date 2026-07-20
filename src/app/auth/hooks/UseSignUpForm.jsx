a23f977868651979182dcc70ac9653b15b66ca63
Harshal Bhoyar <harshalbhoyar84@gmail.com>
2026-07-20 23:19:04 +0530

    Added Toast after sign up page

--- src/app/auth/hooks/UseSignUpForm.jsx
+++ src/app/auth/hooks/UseSignUpForm.jsx
@@ -1,10 +1,16 @@
+import { Toaster } from "@/components/ui/sonner";
 import API_CONFIG from "@/config/api.config";
+import { PATHS } from "@/config/path.config";
 import useMutation from "@/lib/hooks/useMutation";
 import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
 
 const useSignUpForm = () => {
 
-    const {data, pending, mutate} = useMutation(API_CONFIG.AUTH.SIGn_UP, 'POST');
+    const navigate = useNavigate();
+
+    const {data, pending, mutate} = useMutation(API_CONFIG.AUTH.SIGN_UP, 'POST');
 
     const form = useForm({
     defaultValues: {
@@ -18,6 +24,11 @@ const useSignUpForm = () => {
     mutate(data, {
         onSuccess: (response) => {
             console.log(response);
+            toast("Sign up successfull", {
+              description: "Use your credentials to Sign in",
+              type: "success"
+            })
+            navigate(PATHS.SIGN_IN);
         },
         onError: (err) => {
             console.log(err);
--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Router from './router'
+import { Toaster } from '@/components/ui/sonner'
 
 const App = () => {
   return (
     <>
       <Router />
+      <Toaster richColors/>
     </>
   )
 }
